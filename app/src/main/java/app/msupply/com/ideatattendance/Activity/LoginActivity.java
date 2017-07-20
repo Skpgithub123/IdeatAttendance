36a2cb9e0eb6736d3d5213a212b2a29f5cadb509
Zest Developer <gowtham.app@gmail.com>
2017-07-20 11:47:43 +0530

    Initial commit

---
+++ app/src/main/java/app/msupply/com/ideatattendance/Activity/LoginActivity.java
@@ -0,0 +1,20 @@
+package app.msupply.com.ideatattendance.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import app.msupply.com.ideatattendance.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+
+
+
+    }
+}
