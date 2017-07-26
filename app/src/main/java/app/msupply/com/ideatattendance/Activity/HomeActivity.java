03351399386c87b09bf72af5a0c49e480512ec10
Zest Developer <gowtham.app@gmail.com>
2017-07-26 12:30:37 +0530

    in homeactivity removed optionsmenu

--- app/src/main/java/app/msupply/com/ideatattendance/Activity/HomeActivity.java
+++ app/src/main/java/app/msupply/com/ideatattendance/Activity/HomeActivity.java
@@ -85,7 +85,7 @@ public class HomeActivity extends AppCompatActivity
         return true;
     }
 
-    @Override
+    /*@Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -98,7 +98,7 @@ public class HomeActivity extends AppCompatActivity
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
